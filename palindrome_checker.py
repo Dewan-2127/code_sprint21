70b35118c8ea41d3941e365a8ad5c5b73e6301c1
dewan <dev.dewansajol@gmail.com>
2024-01-14 00:08:25 +0100

    Palindrome Checker solved

---
+++ palindrome_checker.py
@@ -0,0 +1,16 @@
+"""
+Palindrome Checker problem
+-solved by Dewan
+"""
+
+
+def is_palindrome(string):
+    # Convert to lowercase and remove anything without characters
+    clean_string = ''.join(char.lower() for char in string if char.isalnum())
+
+    # Compare with reverse string
+    return clean_string == clean_string[::-1]
+
+
+print(is_palindrome("A man, a plan, a canal, Panama"))
+print(is_palindrome("Hello"))
